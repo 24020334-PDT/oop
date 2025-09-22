b6443cc092c3f24aadb6b9a31a281cec2bc7deb4
24020334-PDT <24020334@vnu.edu.vn>
2025-09-23 00:57:37 +0700

    add HW1

---
+++ HW1/Distance.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Distance {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        int y = sc.nextInt();
+
+        double distance = Math.sqrt(x * x + y * y);
+
+        System.out.println("Distance from (" + x + ", " + y + ") to (0, 0) = " + distance);
+    }
+}
