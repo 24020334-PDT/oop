d254c2d801f0e81e47cb942e2b14b25bfc49db57
24020334-PDT <24020334@vnu.edu.vn>
2025-09-23 00:39:33 +0700

    add HW1

---
+++ HW1/Introduction.java
@@ -0,0 +1,14 @@
+public class Introduction {
+
+    public static void main(String[] args) {
+        System.out.println("Phạm Đức Trọng\t24020334\tIT1\t24020334-PDT\t24020334@vnu.edu.com");
+
+        for (int i = 9; i >= 1; i--) {
+                System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+                System.out.println("Take one down, pass it around,");
+        }
+
+        System.out.print("No more bottles of beer on the wall.");
+    }
+
+}
